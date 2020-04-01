b3b05fc0ac3a1a4fa36c6329a47e732af77fdbff
MasayaAoyama <tennis.tenmasa@gmail.com>
2020-04-02 04:09:51 +0900

    mod

--- namespace-pattern/run.sh
+++ namespace-pattern/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BRANCH=master
 
+cd $(dirname $0)
 kubectl create namespace $BRANCH
 sed "s|__BRANCH__|${BRANCH}|g" template.yaml | kubectl apply -f -
 
--- prefix-pattern/run.sh
+++ prefix-pattern/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 BRANCH=master
 
+cd $(dirname $0)
 sed "s|__BRANCH__|${BRANCH}|g" template.yaml | kubectl apply -f -
 
